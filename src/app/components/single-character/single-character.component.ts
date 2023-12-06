657e229d28e03e251e70bd6912836d8a2a355ce7
dariolaseta <dario.laseta@gmail.com>
2023-12-06 09:58:41 +0100

    removed unused data

--- src/app/components/single-character/single-character.component.ts
+++ src/app/components/single-character/single-character.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/services/api.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,9 +15,13 @@ export class SingleCharacterComponent implements OnInit {
   charactersArray: any[] = [];
   id!: number;
 
+  myModel = {};
+
   // Edit
   canEdit: boolean = false;
   editedCharacterIndex: number = -1;
+
+  @Output() updatedArray = new EventEmitter<any>();
 
   constructor(
     private route: ActivatedRoute,
